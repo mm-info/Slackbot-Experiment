94075e31d803bd1763fa187cef64e3fae3cb1d43
Matt Michaels <matt+ga@mattmichaels.info>
2020-02-18 20:15:18 -0600

    Restarted project, need to recreate the app on heroku

--- scripts/example.js
+++ scripts/example.js
@@ -34,35 +34,113 @@
 
 
 module.exports = function(robot) {
-  const attendenceCount = [];
 
-  // FEATURE: Responding to role call
-  robot.respond(/Role call!/i, function(res) {  // Wait for `@matts-hubot role call!`
-    if (attendenceCount.length === 0) {
-      res.send("Please reply with HERE so that we can mark your attendance.");   // Alerts class to reply
-      attendenceCount.push('');
+
+  // Getting variables and arrays setup
+  const attendeeArray = [];   // Empty array
+  let roleCallStarted;        // Falsy value
+  let attendeeCount = 0;      // Variable to add user count to
+
+  // Helper: Get the sender's username.
+  const get_username = response => `@${response.message.user.name}`;
+
+
+  // Feature 1.1: Start role call
+  //
+  // Wait for `@matts-hubot Start role call`
+  robot.respond(/Start role call/i, function(res) {
+
+    // If the roleCallStarted is false...
+    if (roleCallStarted !== true) {
+
+      // 1. Set it to true
+      roleCallStarted = true;
+
+      // 2. Let the class know it has begun
+      res.send("@here Class is starting, time to mark your attendance! \nAttendence can be submitted by sending: `@matts-hubot here`");
     } else {
-      res.send("Attendence is already being taken!");   // Alerts class to reply
+
+      // If the role call already started, let the users know
+      res.send("Attendence is already being taken!");
     }
   });
 
-  // FEATURE: Count amount of students checking in
-  robot.hear(/here/i, function(res) {
-    return res.send("Heard you.");
 
-    if (attendenceCount.length >= 1) {
-      attendenceCount.push('Attendent recorded');
-      return res.send("A total of " + users.length + " students have checked in so far.");
+  // Feature 1.2: Counting and logging user replies
+  //
+  // Listening for `@matts-hubot here`
+  robot.respond(/here/i, function(res) {
+
+    // If the role call has started...
+    if (roleCallStarted) {
+
+      // 1. Add to user count
+      attendeeCount++
+
+      // 2. Push username to array
+      attendeeArray.push(get_username(res));
+
+      // 3. Send personal message
+      res.send("Thanks for coming to class today " + get_username(res));
+
     } else {
-     return res.reply("Sorry, role call is not being counted currently.");
+      // If role call hasn't started yet, fill them in.
+      res.reply("Sorry, role call is not being counted currently.");
     }
   });
 
-  robot.hear(/yyy/i, function(res) {
-    const room = "general";
-    return robot.messageRoom(room, "I do not like green eggs and ham.  I do not like them sam-I-am.");
+  // Feature 1.3: Checking user list
+  //
+  // Listening for `@matts-hubot status?`
+  robot.respond(/Status?/i, function(res) {
+
+
+
+    // 1. Show user count
+    if (attendeeCount > 0) {
+      res.send("The following " + attendeeCount + " student(s) have checked in today.");
+
+      // 2. Loop through, and output each registered username
+      for (var i = 0; i < attendeeArray.length; i++) {
+        res.send(" + " + (`${attendeeArray[i]}`));
+      }
+    } else {
+      res.send("Nobody registered yet...");
+    }
   });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // testing
